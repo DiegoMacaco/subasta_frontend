253c5aa621ded74dc490159c31b4d199a1703a05
DiegoMacaco <diego.guisbert.62103@usalesiana.edu.bo>
2025-10-16 04:19:10 -0400

    feat: implementar Tailwind CSS en el proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,29 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        qhatu: {
+          base: '#89C9B8',
+          dark: '#2E594E',
+          accent: '#EFD780',
+          light: '#F8F9F8',
+          text: '#333333',
+        },
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
